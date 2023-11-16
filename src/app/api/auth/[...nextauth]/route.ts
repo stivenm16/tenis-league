ab29c18831afd24f24e3b9274ec675b88cdd2a5a
Kevin <stiivenmendez16@gmail.com>
2023-11-16 13:29:47 -0500

    jwt

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,59 @@
+import db from '@/libs/prisma'
+import bcrypt from 'bcrypt'
 import NextAuth from 'next-auth'
-import { authOptions } from '../opts'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password', placeholder: '*****' },
+      },
+      async authorize(credentials, req) {
+        const userFound = await db.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        })
+
+        if (!userFound) throw new Error('No user found')
+
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.password,
+        )
+
+        if (!matchPassword) throw new Error('Wrong password')
+
+        return {
+          id: userFound.id,
+          name: userFound.userName,
+          email: userFound.email,
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ account, token, user, profile }) {
+      if (user) token.user = user
+      return token
+    },
+    async session({ session, user }) {
+      const userFound = await db.user.findUnique({
+        where: {
+          email: session.user.email,
+        },
+      })
+      session.user.role = userFound.role
+      session.user.userId = userFound.id
+      return session
+    },
+  },
+  pages: {
+    signIn: '/auth/login',
+  },
+})
 
 export { handler as GET, handler as POST }
